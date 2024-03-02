2f84bfef67d0f1db55fc2d22f0cce9a7de2b88b7
Samuel Sampaio Ribeiro <111511573+samuelsampaio2020@users.noreply.github.com>
2024-03-02 12:41:24 -0300

    alterado

--- Logica_de_Programação/respostas_atividades.py
+++ Logica_de_Programação/respostas_atividades.py
@@ -1,4 +1,5 @@
 from exercicio import *
+#https://github.com/isquicha/exercicios-logica-python/tree/50c9cd9418fae4bccb29144878b216df401a9d60?tab=readme-ov-file#listas
 
 atividade = exercicio()
 atividade.Questao_17()
